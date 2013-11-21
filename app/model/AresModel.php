186b899fa2e9be6ce3fcd877adaf8e1c6279cb99
Michael Zabka <zabka.michael@seznam.cz>
2013-11-21 21:51:09 +0100

    ARES better

--- app/model/AresModel.php
+++ app/model/AresModel.php
@@ -88,38 +88,22 @@ class AresModel {
             return array();
         }
 
-        $ok = preg_match_all('~<are:ICO>(\d+)</are:ICO>~', $response, $ics, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<are:Obchodni_firma>(.+)</are:Obchodni_firma>~', $response, $names, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:Nazev_ulice>(.+)</dtt:Nazev_ulice>~', $response, $streets, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:Cislo_domovni>(\d+)</dtt:Cislo_domovni>~', $response, $cps, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:Cislo_orientacni>(\d+)</dtt:Cislo_orientacni>~', $response, $cos, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:PSC>(\d+)</dtt:PSC>~', $response, $postcodes, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:Nazev_obce>(.+)</dtt:Nazev_obce>~', $response, $obces, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:Nazev_casti_obce>(.+)</dtt:Nazev_casti_obce>~', $response, $castiObces, PREG_SET_ORDER);
-        $ok = $ok && preg_match_all('~<dtt:Nazev_mestske_casti>(.+)</dtt:Nazev_mestske_casti>~', $response, $castiMestas, PREG_SET_ORDER);
-        if (!$ok) {
-            Debugger::log('Nenašlo žádné části v XML', Debugger::WARNING);
-            return array();
-        }
-
-        if (count($ics) !== count($names) || count($names) !== count($streets)
-            || count($streets) !== count($cps) || count($cps) !== count($cos)
-            || count($cos) !== count($postcodes) || count($postcodes) !== count($obces)
-            || count($obces) !== count($castiObces) || count($castiObces) !== count($castiMestas)) {
-            Debugger::log(Json::encode(array($ics, $names, $streets, $cps, $cos, $postcodes, $obces, $castiObces, $castiMestas)));
-            Debugger::log('Některých položek není v XML stejně', Debugger::ERROR);
-            return array();
-        }
+        $xml = \XmlHelper::xml2array($response);
+        $records = $xml['are:Ares_odpovedi']['are:Odpoved']['are:Zaznam'];
 
         $companies = array();
-        foreach ($ics as $i => $notUsed) {
+        foreach ($records as $record) {
             $company = array(
-                'ic' => $ics[$i][1],
-                'name' => $names[$i][1],
+                'ic' => @$record['are:ICO'],
+                'name' => @$record['are:Obchodni_firma'],
                 //'size' => $sizes[$i][1],
-                'address_city' => $obces[$i][1].' - '.$castiObces[$i][1].' - '.$castiMestas[$i][1],
-                'address_street' => $streets[$i][1].' '.$cps[$i][1].'/'.$cos[$i][1],
-                'address_postcode' => $postcodes[$i][1],
+                'address_city' => @$record['are:Identifikace']['are:Adresa_ARES']['dtt:Nazev_okresu'].' - '.
+                    @$record['are:Identifikace']['are:Adresa_ARES']['dtt:Nazev_obce'].' - '.
+                    @$record['are:Identifikace']['are:Adresa_ARES']['dtt:Nazev_casti_obce'],
+                'address_street' => @$record['are:Identifikace']['are:Adresa_ARES']['dtt:Nazev_ulice'].' '.
+                    @$record['are:Identifikace']['are:Adresa_ARES']['dtt:Cislo_domovni'].'/'.
+                    @$record['are:Identifikace']['are:Adresa_ARES']['dtt:Cislo_orientacni'],
+                'address_postcode' => @$record['are:Identifikace']['are:Adresa_ARES']['dtt:PSC'],
             );
             $companies[$this->hash($company)] = $company;
         }
@@ -138,4 +122,5 @@ class AresModel {
     protected function hash(array $x) {
         return sha1(Json::encode($x));
     }
-} 
+
+}
